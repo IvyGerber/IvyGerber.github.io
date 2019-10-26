6a0122594de3fbcbc4e76d4c92a6d0d8e8be733a
Hanna <raynihon@gmail.com>
2019-10-26 18:03:14 +0300

    the count of elements in the basket was completed in js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let productsCountEl=document.getElementById("products-count");
-console.log(productsCountEl)
+console.log(productsCountEl);
+let addToCartButtons=document.querySelectorAll(".add");
+console.log(addToCartButtons);
+for (let i = 0; i < addToCartButtons.length; i++) {
+	addToCartButtons[i].addEventListener("click",function(){
+		let prevProductsCount=+productsCountEl.textContent;
+		productsCountEl.textContent=prevProductsCount+1;
+	})
+}
