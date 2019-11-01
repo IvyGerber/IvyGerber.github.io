1e84275219458acd49a7f9b7fd4a4902b89d82c5
Hanna <raynihon@gmail.com>
2019-11-02 00:46:54 +0200

    added likes and filed background images

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 let productsCountEl=document.getElementById("products-count");
-console.log(productsCountEl);
 let addToCartButtons=document.querySelectorAll(".add");
-console.log(addToCartButtons);
 for (let i = 0; i < addToCartButtons.length; i++) {
 	addToCartButtons[i].addEventListener("click",function(){
 		let prevProductsCount=+productsCountEl.textContent;
 		productsCountEl.textContent=prevProductsCount+1;
 	})
 }
-let productLikeEl=document.getElementsByClassName("like");
-console.log(productLikeEl)
-for (let i = 0; i < productLikeEl.length; i++) {
-	productLikeEl[i].addEventListener("click",function myFunction(x) {
-  x.classList.toggle("like");
+let likeButton=document.getElementsByClassName("like");
+for (let i = 0; i < likeButton.length; i++) {
+	likeButton[i].addEventListener("click",function() {
+  likeButton[i].classList.toggle("liked");
 })
 }
